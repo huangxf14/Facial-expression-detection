0d5366afb992b417937504bfb3f994da93188c99
wangqr <wangqirui1996@gmail.com>
2016-11-16 20:12:41 +0800

    finish 1st part of question 1

--- hw2/hw1.m
+++ hw2/hw1.m
@@ -16,7 +16,7 @@ modSignal = 2.*data - 1;
 
 %% Pulse Forming
 t = 0:1./Rws:N./Rs;
-waveformSingle = rcosfir(alpha,N-0.5,k,1,'sqrt');
+waveformSingle = rcosdesign(alpha,2.*N-1,k,'sqrt');
 modSignalExpanded = complex(zeros(1,N.*k+1));
 modSignalExpanded(k./2+1:k:end) = modSignal;
 waveform = conv(waveformSingle, modSignalExpanded);
@@ -28,7 +28,7 @@ waveformMod = waveform .* cos(2 .* pi .* f0 .* t);
 %% 1. Plot waveform and its spectrum.
 Nsample = 10;
 figure;
-hold('on');
+hold on;
 stem(0.5:1:Nsample,modSignal(1:Nsample).*waveformSingle((end+1)./2),'.');
 plot(0:1./k:Nsample,waveform(1:Nsample.*k+1),'--');
 %plot(0:1./k:Nsample,-waveform(1:Nsample.*k+1),'--');
@@ -36,6 +36,7 @@ plot(0:1./k:Nsample,waveformMod(1:Nsample.*k+1));
 title('Waveform');
 xlabel('t/Ts');
 legend('Symbol','Baseband Waveform','Modulated Waveform');
+drawnow;
 
 figure;
 spectrum = fft(waveformMod);
@@ -43,6 +44,40 @@ fRange = 0:Rws./length(spectrum):4000;
 plot(fRange, abs(spectrum(1:length(fRange)))./max(abs(spectrum)));
 title('Spectrum of Modulated Signal');
 xlabel('f/Hz');
+drawnow;
 
 %% Add Noise
-%TODO
+Eb_n0 = 4;
+SNR = 2 .* Eb_n0;
+waveformRecv = awgn(waveformMod, SNR, 'measured');
+
+%% 2. Plot waveform and its spectrum.
+figure;
+hold on;
+plot(0:1./k:Nsample,waveformMod(1:Nsample.*k+1));
+plot(0:1./k:Nsample,waveformRecv(1:Nsample.*k+1));
+title('Waveform');
+xlabel('t/Ts');
+legend('Sent Waveform', 'Received Waveform');
+drawnow;
+
+figure;
+spectrum = fft(waveformRecv);
+fRange = 0:Rws./length(spectrum):4000;
+plot(fRange, abs(spectrum(1:length(fRange)))./max(abs(spectrum)));
+title('Spectrum of Received Signal');
+xlabel('f/Hz');
+drawnow;
+
+%% Carrier Demodulation
+waveformDemod = waveformRecv .* cos(2 .* pi .* f0 .* t);
+rcosWindow = zeros(1,N.*k./2+1);
+rcosWindow(1:convergent((N.*k+1)./k)) = 1;
+slopeRange = convergent(ceil(1+(1-alpha).*(N.*k+1)./k):floor(1+(1+alpha).*(N.*k+1)./k));
+rcosWindow(slopeRange) = cos(((slopeRange-1).*k./(N.*k+1)-(1-alpha)).*(pi./4./alpha));
+rcosWindow = [rcosWindow, fliplr(rcosWindow(2:end))];
+waveformDemod = ifft(rcosWindow.*fft(waveformDemod));
+dataRecv = waveformDemod(k./2+1:k:end) >= 0;
+
+%% Calculate BER
+fprintf('BER = %f at Eb/n0 = %.2fdB.\n',sum(dataRecv~=data)./N,Eb_n0);
