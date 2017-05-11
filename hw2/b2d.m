a17c73e083b806b0c20968e8f6d6cd71d7c73349
huangxf14 <464271301@qq.com>
2017-05-11 15:55:11 +0800

    delelte trash

--- hw2/b2d.m
+++
@@ -1,9 +0,0 @@
-function [ out ] = b2d( in )
-%B2D Summary of this function goes here
-%   Detailed explanation goes here
-out = reshape(in,[],8);
-%out = out*[128,64,32,16,8,4,2,1]';
-out = out*[1,2,4,8,16,32,64,128]';
-out = uint8(out);
-end
-
--- hw2/fconv.m
+++
@@ -1,7 +0,0 @@
-function [y]=fconv(x, h)
-% Fast Convolution, same result as conv(x, h)
-
-Ly=length(x)+length(h)-1;
-Ly2=pow2(nextpow2(Ly));
-y=ifft(fft(x, Ly2).*fft(h, Ly2));
-y=y(1:Ly);
