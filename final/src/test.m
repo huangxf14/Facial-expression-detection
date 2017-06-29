36cd2bc7244c2c453926840234816d193428950a
huangxf14 <464271301@qq.com>
2017-06-29 20:19:47 +0800

    add more algorithm

--- final/src/test.m
+++ final/src/test.m
@@ -5,7 +5,11 @@ weight=[0.1,1,1,0.2,0.2,1,1,0.1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,0.6,0.6,1,
 epr=['an';'co';'di';'fe';'ha';'ne';'sa';'su'];
 file=dir('test/公开数据测试集');
 file=file(3:end);
-for filecnt=1:length(file)
+labelcnt=zeros(1,8);
+for x=1:8
+    labelcnt(x)=length(find(label==char(x+47)));
+end;
+for filecnt=12:12%length(file)
     img=imread(['test/公开数据测试集/' file(filecnt).name]);
     lab=floor(rand(1,1)*8);
     min=100000000000000;
@@ -14,7 +18,6 @@ for filecnt=1:length(file)
     
     if (~isempty(bbox))
        disp('true');
-       disp(filecnt);
        for k=1:size(bbox,1)
           box=bbox(k,:);
           pic=img(box(2):box(2)+box(4)-1,box(1):box(1)+box(3)-1,:);
@@ -33,13 +36,16 @@ for filecnt=1:length(file)
               dis=[dis,chi_square_dis(feature(cnt,:),newfeature)];
           end;
           [sortdis,index]=sort(dis);
-          for i=1:60
+          for i=1:50
               cal(label(index(i))-47)=cal(label(index(i))-47)+1;
           end;
         end;
       end;
+      imshow(img);
+      cal=cal./labelcnt;
       [sortcal,index]=sort(cal);
       if (sortcal(end)~=0) lab=index(end)-1;end;
+      disp(epr(lab+1,:))
       fid=fopen('公开数据结果.txt','a');
       fprintf(fid,'%s\n',[file(filecnt).name ' ' epr(lab+1,:)]);
       fclose(fid);
